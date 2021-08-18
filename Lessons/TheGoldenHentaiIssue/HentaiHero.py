ab02bb336346f21a23097c6d87f6c8fb20c0f52c
Genesis <daintygenesis@gmail.com>
2021-08-18 14:27:19 -0500

    Added Hentai Hero WIP

---
+++ Lessons/TheGoldenHentaiIssue/HentaiHero.py
@@ -0,0 +1,109 @@
+""" 
+
+█▀▀ █▀▀ █▄░█ █▀▀ █▀ █ █▀   █░░ █▀▀ █▀ █▀ █▀█ █▄░█ █▀   █░█ █▀█ █░░ █░█ █▀▄▀█ █▀▀   ▀█
+█▄█ ██▄ █░▀█ ██▄ ▄█ █ ▄█   █▄▄ ██▄ ▄█ ▄█ █▄█ █░▀█ ▄█   ▀▄▀ █▄█ █▄▄ █▄█ █░▀░█ ██▄   █▄
+Welcome to the genesis gir lesson tutorials Volume 1! 
+. Thanks for downloading! The Golden Hentai Issue is a program that takes the user threw a
+situation where they need to enter the correct username and password to obtain the goods! can
+you do it or will you fail and never see the japanese fandom? well try to guess the correct
+creds and of course hints will be given throughout the program to better assist you! Goodluck you
+pervert! 
+⼕ㄖᗪ🝗ᗪ & 山尺讠セセ🝗𝓝 ⻏丫 Ꮆ🝗𝓝🝗丂讠丂 Ꮆ讠尺
+"""
+# Guess the password to obatain the comic and gain ownership of the hentai!
+
+import sys
+import random
+
+# Authorazation
+auth = False
+
+# items
+hentai_book = 'Hentai Comics (Story Items)'
+
+# Characters
+Asuka = 'Asuka'  # The guide of the program!
+Old_Man = 'Old Man'
+Miya = 'Miya'
+
+# Program starts here
+print('#🅶🅴🅽🅴🆂🅸🆂 🅶🅸🆁 🅶🅴🅽🅴🆂🅸🆂 🅶🅸🆁 🅶🅴🅽🅴🆂🅸🆂 🅶🅸🆁 🅶🅴🅽🅴🆂🅸🆂 🅶🅸🆁 🅶🅴🅽🅴🆂🅸🆂 🅶🅸🆁')
+print()
+print('                        ██╗░░██╗███████╗███╗░░██╗████████╗░█████╗░██╗  ██╗░░██╗███████╗██████╗░░█████╗░')
+print('                        ██║░░██║██╔════╝████╗░██║╚══██╔══╝██╔══██╗██║  ██║░░██║██╔════╝██╔══██╗██╔══██╗')
+print('                        ███████║█████╗░░██╔██╗██║░░░██║░░░███████║██║  ███████║█████╗░░██████╔╝██║░░██║')
+print('                        ██╔══██║██╔══╝░░██║╚████║░░░██║░░░██╔══██║██║  ██╔══██║██╔══╝░░██╔══██╗██║░░██║')
+print('                        ██║░░██║███████╗██║░╚███║░░░██║░░░██║░░██║██║  ██║░░██║███████╗██║░░██║╚█████╔╝')
+print('                        ╚═╝░░╚═╝╚══════╝╚═╝░░╚══╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝  ╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝░╚════╝░')
+print()
+print('                                                                                                                        ')
+print('                                                                                                                        ')                
+print('                               `.-://////:-`                                  `.-:::::--.`                              ')                
+print('                         .:/oyyyyssoo+//::-/:`                              .//://+oossyyhhys+:.`                       ')                
+print('                    `-/+oo/:-.```                                                      ````.-:+ooo/:.                   ')                
+print('                `-:/:-.`          `````                                           ```````        `.-:/:-.               ')                
+print('             .-::.`         `````.....:/.``                                    ``-:....````````       `.-/:.`           ')                
+print('          `.-.`          `.--:::::---::+sh:                                   `y++:/+:::///:::--.`        `.--`         ')                
+print('          `           `..-...`  ``.-+//shs/```-/`                        ::.` `smy+-+/::-+   `..-:-`          ``        ')                
+print('                     ``    -..:ohhmNNmNNhssyyy+.                         `/yyyooyNNNmNNNmmho-`                          ')                
+print('                      -``./dhmNNNNNNNNNNNNNy:`                             `:smNNNNNNNNNNNNNNds//`                      ')                
+print('               ```` ``:hdmNNNNNNNNNNNNNNNNNs`                               -omNNNNNNNNNNNNNNNNNmho:-.                  ')                
+print('               .---+hdNNNNNNNNNNNNNNNNNNNNNms.                              +mNNNNNNmmNNNNNNNNNNNNNNmo-.-:/+o/`         ')                
+print('              ``-/yNNNNNNNNNmddmNNNNNNNNNNNdo/                             -+hNNNmy+::/yNNNNNNNNNNNNNNNNNho/-`          ')                
+print('     .+sssssyydmNNNNNNNNNmy/.``:dNmmNNNNNNNNs                               /mNNh-     `hNmmNNNNNNNNNNNNNd/`            ')                
+print('       `.:/+++hNNNNNNNNNh.      yh//mNNNNNmsms                             /myhd`      .dd++dNNNNNNNNNNNNNNdo:-`        ')                
+print('        ````:hNNNNNNdyNh`      :y+smNNNNNNN+`/-                           `+.`m+      -/:/yymNNNNNNmyNNNNNNNNmds/--`    ')                
+print('        `:yNNNNNNNd/-sN-      -. `yNNNNNNNNy                                 -N-    `s. `oNNNmmmmmNm::hNNNNNNNmmmmho/-  ')                
+print('      `:yNNmmNNNNy.``sN/    `/h-:smNNmmmmmmy                                 -Ns-..+y/ossdNNmmmmmmmN-..oNNNNNy`.::-:+o+.')                
+print('    .odmho:/oNNNy````sNmyooysyhm+`omNmmmmdmy                                 .mdddhhsoys.:hdddddddmm.```/NNNN-          ')                
+print('   :o+-``.:``mNNs    :Nmmmdhyyys/:osysssyyd+                                  yyysssyhyy+syysooshhmy    /NNNs           ')                
+print('             +NNh     ymysso++osyyysssyyyyd.                                  oysso++osyyyssossoosm-    +NNm.           ')                
+print('             `dNh     `hhyy+-.----:-:+osyys                                   -hso/-...-::--:+sssd/     oNN/            ')                
+print('              /ms      .yyys/-...``../ssyy.                                    /yso:-..````.:ossy:      /No`            ')                
+print('              `:+``.`   `+ysso/::::/+ssso.                                      /ssso/::::/+ssys-   `...:+.`            ')                
+print('               .+odddhyo+/ohyyyyyyyso+/-`                                        .:+osyyyyyyyyo/+osyhddyo/.`            ')                
+print('               --+/oyydhhhhyyyso/:--``                                              `.-:/+syyyhhhhhyys+/--`             ')                
+print('                `` -``.```````                                                            ```````````````               ')                
+print('                    ``````````                                                            ``````````````````            ')                
+print('                ``````````````                                                             `````````````````            ')                
+print('                 `````````````                                                             `````````````````            ')                
+print('                 ````````````                                                               ```````````````             ')                
+print('                   ```````                                                                    ```````````               ')                
+print('                                                                                                `````                   ')                                                                                                                             
+print()
+print()
+print(Asuka+':Hey welcome to the Database center! Here youll be prompted a series of questions')
+print('to be able to enter the vault and gain access to the lush and amazing '+hentai_book+'!')
+prompt = input('(Press enter to play!)\n')
+print(Asuka+':Create an account?')
+prompt = input('(y/n)\n')
+if prompt=='y':
+    print(Asuka+':Create a username.\n')
+    user = input()
+    print()
+    print(Asuka+':Create a password.\n')
+    password = input()
+    print()
+
+    #Shows user Login info
+    print('Login info!')
+    print('Username: '+user+'!\n')
+    print('Password: '+password+'!\n')
+
+    #Prompts user to enter login 
+    print(Asuka+':Login into the Database with your info please.')
+    prompt=input('(Press enter)\n')
+
+    while True:
+        print('System:Enter your username login\n')
+        login=input()
+        if login==user:
+            print('Perfect!')
+            auth=True
+            break
+        elif login!=user:
+            print('Incorrect login info!\n')
+            continue
+elif prompt=='n':
+    print('System:okay buh bye!')
+    sys.exit
