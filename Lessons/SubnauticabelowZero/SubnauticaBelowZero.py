b8d229cb8fc95e6ff66dc7323497e44caaa0c92e
genesisgir <daintygenesis@gmail.com>
2022-04-09 16:45:35 -0400

    added audio files to Subnautica: Below zero!

--- Lessons/SubnauticabelowZero/SubnauticaBelowZero.py
+++ Lessons/SubnauticabelowZero/SubnauticaBelowZero.py
@@ -27,8 +27,7 @@ among the debris paint a new picture of the incident. With limited resources, yo
 Survive the harsh climate by constructing extensive habitats, scavenging for resources, and crafting equipment. Blast
 across the snowy tundra on a Snowfox hoverbike. Cruise through enchanting and perilous biomes in your modular Seatruck.
 
-Research alien lifeforms
-
+𝙍𝙚𝙨𝙚𝙖𝙧𝙘𝙝 𝙖𝙡𝙞𝙚𝙣 𝙡𝙞𝙛𝙚𝙛𝙤𝙧𝙢𝙨
 Something undiscovered lurks around every corner. Swim through the giant Titan Holefish, encounter the haunting Shadow Leviathan,
 and visit the adorable Pengwings. Keep your wits about you. Not all creatures in this strange world are friendly.
 
@@ -42,7 +41,9 @@ and visit the adorable Pengwings. Keep your wits about you. Not all creatures in
 # program starts at line '18'
 
 # importing modules
-import webbrowser,sys,time
+import webbrowser,sys,time,random,winsound
+from playsound import playsound 
+
 
 # defining variables/constants
 
@@ -62,7 +63,7 @@ quartz_ore = "Quartz" # SiO4. Silica in crystalline form.
 ruby_ore = "Ruby" # # Al2O3. Hard, conductive gemstone composed of aluminum oxide. 
 salt_deposit_ore = "Salt Deposit" # NaCl. Culinary and sanitation applications.  
 silver_ore = "Silver Ore" # Ag. Conductive element, and microbicide. 
-Snowball = "Snowball" # 
+snowball = "Snowball" # 
 titanium_ore = "Titanium" # Ti. Basic building material. 
 uraninite_crystal = "Uraninite Crystal" # U3O8. A highly radioactive material.
 
@@ -204,12 +205,347 @@ tether_tool = "Tether Tool" # Teleport to a nearby teleportation seatruck module
 thermoblade = "Thermoblade" # Cooks and sterilizes small organisms for immediate consumption. 
 thumper = "Thumper" # Makes thumping noises to confuse ice worms.
 
-# 
+# vehicle constants
+cyclops = "Cyclops" # refer to line '213'
+prawn_suit = "Prawn Suit" # refer to line '218'
+seamoth = "Seamoth " # refer to line '223'
 
+"""
+𝙘𝙮𝙘𝙡𝙤𝙥𝙨
+Be advised: the Cyclops is designed to be operated by a three-person crew. Only experienced helms-people should
+attempt to pilot this vehicle solo. 
+"""
 
+"""
+𝙋𝙧𝙖𝙬𝙣 𝙎𝙪𝙞𝙩
+The Exosuit facilitates mobility in high-pressure and zero-gravity environments, turning the wearer into a bipedal
+mechanical rock puncher — rock grub ingestion not required. 
+"""
 
+"""
+𝙎𝙚𝙖𝙢𝙤𝙩𝙝
+The Seamoth is a small, versatile one-person submersible and spacecraft capable of sustained high-speed travel
+over considerable distances. It boasts an omnidirectional propulsion system that allows for lateral and vertical
+movement that gradually increases to a maximum of 11.25 m/s in any single direction. The speed in any direction
+is independent of other directions and can be added together using vector addition. Thus, if going forward and
+upward the maximum speed is 15.91 m/s (11.25 * √2) and if going forward, upward, and sideways the maximum speed
+is 19.49 m/s (11.25 * √3). 
+"""
 
+# world constants
+planet_4546B  = "Planet 4546B" # Planet 4546B is a category-3 ocean planet on which the story of Subnautica and Subnautica: Below Zero takes place.
 
+# character constants
+robin_ayou = "Robin Ayou" # refer to line '241'
 
+"""
+𝙍𝙤𝙗𝙞𝙣 𝘼𝙮𝙤𝙪
+Robin is a Xenoworker and Xenologist. She arrived to 4546B with the intent of uncovering what truly happened to
+her sister Sam Ayou after the Alterra Corporation reported her to have died. 
+"""
+
+# health, hunger variable amounts
+health_amounts = str(100)
+hunger_amounts = str(100)
+
+health_amounts += "%"
+hunger_amounts += "%"
+
+"""
+
+█░█ ▄▀█ █▀█ █ ▄▀█ █▄▄ █░░ █▀▀ █▀
+▀▄▀ █▀█ █▀▄ █ █▀█ █▄█ █▄▄ ██▄ ▄█
+
+Variables are useful in the sense you can store data types & values within them to use in your program later on,
+variables and constants have their differences in a way. variables values change throughout a program and constants
+never change their values of the entire instance of a program! you make variables/constnats with assignment
+statements refer to the examples below to learn how to make them.
+
+eg. rusty_crate = "plasma rifle"
+
+the statement above is an assignment statement and consists of a variable/constant name, an assigment operator,
+and the value that you wish to store inside its contents. use them , manipulate them, harness their capabilities.
+
+      v a r i a b l e s
+                              o \ 0 1 0 1 0 1 o \
+                              n   \           n   \
+         c o n s t a n t s     s     \         s     \
+                              t       v a r i a b l e s
+                              a       o       a       o
+                              n       n       n       n      𝙩𝙞𝙥: 𝙘𝙤𝙢𝙥𝙞𝙡𝙚𝙧𝙨 𝙖𝙧𝙚 𝙩𝙧𝙖𝙣𝙨𝙡𝙖𝙩𝙞𝙤𝙣 𝙢𝙖𝙘𝙝𝙞𝙣𝙚𝙨
+                              t       s       t       s
+                              s       t       s       t         𝙖𝙙𝙫𝙞𝙘𝙚: 𝙛𝙤𝙘𝙪𝙨 𝙤𝙣 𝙮𝙤𝙪𝙧 𝙘𝙧𝙖𝙛𝙩 𝙖𝙣𝙙 𝙨𝙠𝙞𝙡𝙡𝙨
+                              v a r i a b l e s       a         
+                                \     n         \     n             𝙖𝙙𝙫𝙞𝙘𝙚: 𝙪𝙨𝙚 𝙫𝙖𝙧𝙞𝙖𝙗𝙡𝙚𝙨 𝙩𝙤 𝙨𝙩𝙤𝙧𝙚 𝙙𝙖𝙩𝙖
+                                  \   t           \   t       
+                                    \ s             \ s               
+                                      v a r i a b l e s
+"""
+
+print() # (ELOC)
+print() 
+print("░██████╗██╗░░░██╗██████╗░███╗░░██╗░█████╗░██╗░░░██╗████████╗██╗░█████╗░░█████╗░██╗ ██████╗░███████╗██╗░░░░░░█████╗░░██╗░░░░░░░██╗")
+print("██╔════╝██║░░░██║██╔══██╗████╗░██║██╔══██╗██║░░░██║╚══██╔══╝██║██╔══██╗██╔══██╗╚═╝ ██╔══██╗██╔════╝██║░░░░░██╔══██╗░██║░░██╗░░██║")
+print("╚█████╗░██║░░░██║██████╦╝██╔██╗██║███████║██║░░░██║░░░██║░░░██║██║░░╚═╝███████║░░░ ██████╦╝█████╗░░██║░░░░░██║░░██║░╚██╗████╗██╔╝")
+print("░╚═══██╗██║░░░██║██╔══██╗██║╚████║██╔══██║██║░░░██║░░░██║░░░██║██║░░██╗██╔══██║░░░ ██╔══██╗██╔══╝░░██║░░░░░██║░░██║░░████╔═████║░")
+print("██████╔╝╚██████╔╝██████╦╝██║░╚███║██║░░██║╚██████╔╝░░░██║░░░██║╚█████╔╝██║░░██║██╗ ██████╦╝███████╗███████╗╚█████╔╝░░╚██╔╝░╚██╔╝░")
+print()
+print("███████╗███████╗██████╗░░█████╗░")
+print("╚════██║██╔════╝██╔══██╗██╔══██╗")
+print("░░███╔═╝█████╗░░██████╔╝██║░░██║")
+print("██╔══╝░░██╔══╝░░██╔══██╗██║░░██║")
+print("███████╗███████╗██║░░██║╚█████╔╝")
+print("╚══════╝╚══════╝╚═╝░░╚═╝░╚════╝░")
+print() # (ELOC)
+print() 
+print("all rights & copyrights go to developed by unknown worlds entertainment")
+print()
+print()
+winsound.PlaySound(r"C:\Users\daint\Documents\github\Genesis-Gir-Lessons-Volume-2\Lessons\SubnauticabelowZero\Subnautica - Lifepod\Pod\connetc.wav", winsound.SND_ASYNC)
+# using the time.sleep() function call to cause a delay in the program execution!
+time.sleep(5)
+
+print("The year is 2045 and time and technology has drastically changed and time as we know it has become \n"
+      + f"more advanced but for {robin_ayou} shes on her way on a collision course to {planet_4546B} a planet\n"
+      + "inhabitated by creatures and a deep blue ocean with mysterious animals.\n\n")
+
+time.sleep(7)
+
+print("You will need to survive this planet and seek shelter and gather resources in order to survive")
+print("survival is key in this terrarin and keeping yourself from starving will ensure you stay alive.")
+print()
+
+time.sleep(2)
+
+print(f"You will be crashing to the surface of {planet_4546B} soon prepare yourself!")
+print("(press enter to continue!)\n")
+
+input() # U/I
+
+print()
+print("█▀ █░█ █▄▄ █▄░█ ▄▀█ █░█ ▀█▀ █ █▀▀ ▄▀█ ▀ █▄▄ █▀▀ █░░ █▀█ █░█░█   ▀█ █▀▀ █▀█ █▀█")
+print("▄█ █▄█ █▄█ █░▀█ █▀█ █▄█ ░█░ █ █▄▄ █▀█ ▄ █▄█ ██▄ █▄▄ █▄█ ▀▄▀▄▀   █▄ ██▄ █▀▄ █▄█\n\n")
+print()
+
+while True:
+    print("Welcome to Subnautica: BELOW ZERO!\n")
+    
+    print("start new journey [j]")
+    print("exit game [x]")
+    resp = input()
+
+    if resp == "j":
+        print("Create world name\n")
+        
+        world_name = input() # creation of world name constant!
+        print() # (ELOC)
+        
+        winsound.PlaySound(r"C:\Users\daint\Documents\github\Genesis-Gir-Lessons-Volume-2\Lessons\SubnauticabelowZero\Subnautica - Lifepod\Pod\generate.wav", winsound.SND_ASYNC)
+        
+        # generating world
+        print(f"Generating {world_name} ..\n")
+        time.sleep(random.randint(0,6))
+        
+        print("building virual meshes..\n")
+        time.sleep(random.randint(0,6))
+        
+        print("Time fragments being implemented..\n")
+        time.sleep(random.randint(1,6))
+        
+        print("The ship is preparing for collision..\n")
+        time.sleep(random.randint(0,6))
+        
+        print(f"{world_name} is being rendered 15%\n")
+        time.sleep(random.randint(0,6))
+        
+        print(f"{world_name} is being rendered 25%\n")
+        time.sleep(random.randint(0,4))
+        
+        print(f"{world_name} is being rendered 35%\n")
+        time.sleep(random.randint(0,2))
+        
+        print(f"{world_name} is being rendered 45%\n")
+        time.sleep(random.randint(0,4))
+        
+        print(f"{world_name} is being rendered 55%\n")
+        time.sleep(random.randint(0,4))
+        
+        print(f"{world_name} is being rendered 65%\n")
+        time.sleep(random.randint(0,2))
+        
+        print(f"{world_name} is being rendered 75%\n")
+        time.sleep(random.randint(0,4))
+        
+        print(f"{world_name} is being rendered 85%\n")
+        time.sleep(random.randint(0,4))
+        
+        print(f"{world_name} is being rendered 95%\n")
+        time.sleep(random.randint(0,4))
+        
+        print(f"{world_name} is being rendered 100%\n")
+        time.sleep(random.randint(0,3))
+        
+        print(f"{world_name} has been created!\n")
+        
+        print("SYSTEM: LAUNCHING DISTRESS SIGNAL. . .\n")
+        
+        # play radio.wav using winsound/playsound modules!
+        playsound(r"C:\Users\daint\Documents\github\Genesis-Gir-Lessons-Volume-2\Lessons\SubnauticabelowZero\Subnautica - Lifepod\Pod\radio.wav")
+        
+        time.sleep(1)
+        print("SYSTEM: LOCALIZING DATA. . .")
+        time.sleep(1)
+        print("SYSTEM: PREPARE YOURSELF FOR SURVIVIAL. . . \n")
+        
+        print(f"{robin_ayou}:The Systems are failing I think I wont make it!")
+        print("(Press enter to continue)\n")
+        
+        input() # U/I
+        
+        print("- The sounds of blazing fire ripping apart the craft can be heard and your heading for a full-on collision")
+        print(f"course straight to {planet_4546B}! -")
+        print(">>> [press enter]\n")
+        
+        input() 
+        
+        print(f"{robin_ayou}:Set coordinates to x 456, y 25675.3, z 2533.389!\n")
+        
+        print(">>> [press enter]\n")
+        
+        input() 
+        
+        print(f"{robin_ayou}: PERFECT! that should be a nice location to land!")
+        print(">>> [press enter]\n")
+        
+        input() 
+        
+        print("- The alien like craft heads faster and faster to the planets atmosphere in such a defying manner -")
+        print(">>> [press enter]\n")
+        
+        input() # U/I
+        
+        print("- as time slows down you feel the craft ripping apart as the shrapnel turns into metallic glitter -")
+        print(">>> [press enter]\n")
+        
+        input() # U/I takes users input and evaluates to the string equal to users input
+        
+        print(f"{robin_ayou}:Okay its time, this is going to hurt!")
+        print(">>> [press enter]\n")
+        
+        input() # U/I
+        print("- FLAMES AND THE SOUND OF THE ROARING CRAFTS ENGINE EXPLODE-\n")
+        
+        time.sleep(5) # time.sleep() call delays program execution by set integers.
+        
+        print("- METAL AND THE EMOTIONAL MUSIC INTENSIFIES -\n")
+        
+        time.sleep(5)
+        
+        print("- The craft barely stays intact and you reach the atmosphere of " + planet_4546B +"... -\n")
+        
+        time.sleep(5)
+        
+        print("- THE CRAFT LANDS INSIDE OF A BODY OF PLANETS OCEAN! -\n")
+        
+        time.sleep(5)
+        
+        # Player lands on planet4546B on line '360'
+        print("- An alarm siren is going off!-")
+        time.sleep(5)
+        print(f"- {robin_ayou} looks at her hands and sees that shes alive and well.. -\n")
+        print(f"{robin_ayou}: IM ALIVE! DEAR GOD IM ALIVE THANK YOU SCIENCE!")
+        print(">>> [press enter]\n")
+        
+        input() # U/I
+        
+        print("- The craft begins to flood with water and " + robin_ayou + " looks down at her feet shocked -\n")
+        print(f"{robin_ayou}: Im Alive but not for long if I dont escape\n")
+        
+        print("-Checkpoint reached!-")
+        
+        # play checkp.wav with the winsound + playsound modules
+        winsound.PlaySound(r"C:\Users\daint\Documents\github\Genesis-Gir-Lessons-Volume-2\Lessons\SubnauticabelowZero\Subnautica - Lifepod\Pod\checkp.wav", winsound.SND_ASYNC)
+        
+        print(">>> [press enter to continue]\n")
+        
+        input() # U/I
+        
+        break # break statement that escapes the while loop                    
+    elif resp == "x": # elif resp is == to "x" close the program!
+        print("Thank you for trying out the program!\n")
+    
+        print("Make sure to support the creators & developers of Subnatica: BELOW ZERO!\n")
+    
+        print("You will be directed to the subnautica:below zero steam page!\n")
+        time.sleep(3)
+        webbrowser.open("https://store.steampowered.com/app/848450/Subnautica_Below_Zero/") # directs user to steam page
+        time.sleep(3)
+        print("shutting down Subnautica: BELOW ZERO")
+        time.sleep(1)
+        sys.exit() # terminates program.
+    else: # choose a valid option please.
+        print()
+        print("(1) System notification: Please choose a valid option [j] or [x]\n")
+        continue
+    
+while True:
+    
+    winsound.PlaySound(r"C:\Users\daint\Documents\github\Genesis-Gir-Lessons-Volume-2\Lessons\SubnauticabelowZero\Subnautica - Lifepod\Pod\breach.wav", winsound.SND_ASYNC | winsound.SND_ALIAS)
+    
+    print("- WARNING: HULL BREACH! - \n")
+    
+    print("Kick open the glasswindshield! [k]")
+    print("Try to control the craft! [c] \n")
+    
+    choice = input() # creates variable from users input and stores it in 'choice'
+    
+    if choice == "k": # player kicks open the glasswindshield with [k]!
+        print(f"{robin_ayou}: I need to escape! \n")
+        
+        print(f"- {robin_ayou} begins to kick the glasswindshield -\n")
+        input(">>> press [enter]\n")
+        
+        #play escape.wav
+        winsound.PlaySound(r"C:\Users\daint\Documents\github\Genesis-Gir-Lessons-Volume-2\Lessons\SubnauticabelowZero\Subnautica - Lifepod\Pod\escape.wav", winsound.SND_ASYNC)
+        
+        print("- You kick the door open for a hope to escape the drowing craft! -")
+        print("(Press enter) \n")
+        
+        input()
+        
+        for kicks in range(6): # using a for loop to execute the amount determined by the range()
+            print(f"{robin_ayou} kicks the door (" + str(kicks) + ") times! \n")
+            
+            time.sleep(1)
+        
+        print("- The door opens and water begins to rush in as you swim to the surface of the ocean! - \n")
+        time.sleep(5)
+        print("- she swims holding in her breath wanting to reach the surface - \n")
+        time.sleep(5)
+        print(f"{robin_ayou}: *GASP!* \n")
+        time.sleep(5)
+        print("- You look around you and begin swimming to the shore as your drenched in sea water")
+        print(", the weather is in a blizzard state and you must warm yourself.")
+        print("(Press enter) \n")
+        
+        input()
+        
+        break                    
+    elif choice == "c": # player decides to control the ship and meets their deadly fate!
+        
+        print(f"{robin_ayou}:There may be a chance to save this craft!\n")
+        
+        print("- you try and try to dabble with the controls but its no use -\n")
+        
+        time.sleep(1)
+        print("- The craft begins to sink on you die proudly along with it -")
+        time.sleep(5)
+        print("[Loading most recent checkpoint..]")
+        continue
+    else: # if user types anything other than given options.
+        continue
+
+# user reaches the shore of planet 4546B on line '475'
+print(f"{robin_ayou}: I made it..i should look for resources if i want to make it..\n")
 
 
